d036649412f916a38265ac62dab01fe62e137c41
Richard Kalvaitis <rkalvait@emich.edu>
2015-11-07 18:30:41 -0500

    Added comment detection

---
+++ salty.js
@@ -0,0 +1,8 @@
+document.body.style.border = "5px solid red";
+comments = document.getElementsByClassName("md");
+for (var i=0; i<comments.length; i++) {
+	comments[i].innerHTML = comments[i].innerHTML + "CZECH THIS COMMENT BRO!";
+}
+//for (var i=0; i<list.length; i++) {
+//	list[i].innerHTML = list[i].innerHTML + "THIS IS A COMMENT BTW";
+//}
